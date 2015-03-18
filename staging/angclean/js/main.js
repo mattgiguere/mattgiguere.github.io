2801017aae3b48128e898a567f10ad101e00afe9
Matt Giguere <matthew.giguere@gmail.com>
2015-03-18 17:54:57 -0400

    templts, prtls, imgs added
    the page was further broken down into its components.
    
    - A template was added for the footer
    - partial was added for about
    - partial was added for blog
    - partial was added for blog_item
    - partial was added for cv
    - partial was added for research
    - partial was added for sites
    - banner images were added
    - about partial was filled in with a carousel

--- staging/angclean/js/main.js
+++ staging/angclean/js/main.js
@@ -11,13 +11,12 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
     .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
     .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    .when("/cv", {templateUrl: "partials/cv.html", controller: "PageCtrl"})
+    .when("/research", {templateUrl: "partials/research.html", controller: "PageCtrl"})
+    .when("/sites", {templateUrl: "partials/sites.html", controller: "PageCtrl"})
+    // Blog
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
@@ -37,7 +36,7 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
 
   // Activates the Carousel
   $('.carousel').carousel({
-    interval: 5000
+    interval: 50000
   });
 
   // Activates Tooltips for Social Links
