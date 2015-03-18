5c73010ffb91e26559dfe39f8b263ae237eb2b98
Matt Giguere <matthew.giguere@gmail.com>
2015-03-18 18:14:51 -0400

    cleaned about and made home
    I cleaned up the about partial:
    - removed indicators
    - removed content from banner
    - removed arrows to cycle through banner images
    
    I also added a new partial for the home page content.

--- staging/angclean/js/main.js
+++ staging/angclean/js/main.js
@@ -34,13 +34,10 @@ app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
 
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 50000
-  });
 
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
+    // Activates Tooltips for Social Links
+    $('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
-  })
+    });
 });
+
